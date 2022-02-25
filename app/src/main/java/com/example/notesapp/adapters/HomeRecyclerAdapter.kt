8d4c00eea340ac050d24a46af44439880a8a8eec
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-02-25 18:14:33 +0530

    created boundary for selected note card and handled its visibility

--- app/src/main/java/com/example/notesapp/adapters/HomeRecyclerAdapter.kt
+++ app/src/main/java/com/example/notesapp/adapters/HomeRecyclerAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.notesapp.adapters
 
+import android.content.res.Resources
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notesapp.R
 import com.example.notesapp.dataModels.Notes
@@ -26,13 +30,24 @@ class HomeRecyclerAdapter(private var notesList: MutableList<Notes>) :
     override fun getItemCount() = notesList.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var noteHeader = itemView.findViewById<TextView>(R.id.note_header_text_view)!!
-        var noteBody = itemView.findViewById<TextView>(R.id.note_body_text_view)!!
+        private var noteCard = itemView.findViewById<ConstraintLayout>(R.id.note_card)
+        private var noteHeader = itemView.findViewById<TextView>(R.id.note_header_text_view)
+        private var noteBody = itemView.findViewById<TextView>(R.id.note_body_text_view)
+        private var noteSelected = itemView.findViewById<ImageView>(R.id.note_selected_icon)
 
         fun bind(holder: ViewHolder, note: Notes) {
 
             holder.noteHeader.text = note.head
             holder.noteBody.text = note.body
+            holder.noteSelected.visibility =
+                if (note.isSelected) {
+                    holder.noteCard.setBackgroundResource(R.drawable.note_selected_boundary)
+                    View.VISIBLE
+                } else {
+                    holder.noteCard.setBackgroundResource(android.R.color.transparent)
+                    View.GONE
+                }
+
         }
 
     }
